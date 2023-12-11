7d5447ffc4849aa64cb24310f96416f4a16c0c5b
kumashir0 <148173359+kumashir0@users.noreply.github.com>
2023-12-11 10:01:42 +0900

    Update presen.js

--- presen.js
+++ presen.js
@@ -9,7 +9,14 @@ document.addEventListener(`DOMContentLoaded`, function () {
     dateElemant.textContent = currentDate.toLocaleDateString(`ja-JP`, options);
 });
 
-
+//希望納期
+function showSelectedDate() {
+    let datePicker = document.getElementById("datePicker");
+    let selectedDate = new Date(datePicker.value);
+    let options = {year: "numeric", month: "long", day: "numeric"};
+    let formattedDate = selectedDate.toLocaleDateString("ja-JP", options);
+    datePicker.textContent = selectedDate.toLocaleDateString("ja-JP", options);
+}
 
 
 //ドロップダウンを非表示にしておく
@@ -24,6 +31,7 @@ selectCompany.addEventListener("change", function () {
         selectCompanyValue === "PPES解析・構造" ||
         selectCompanyValue === "PPES合成") {
         selectgroupTMC.style.display = "none";
+        selectgroupTMC.selectedIndex = 0;
         selectgroupPPES.style.display = "block";
         selectgroupPPES.style.opacity = 1;
     } else if (
@@ -31,6 +39,7 @@ selectCompany.addEventListener("change", function () {
         selectCompanyValue === "TMC解析・構造" ||
         selectCompanyValue === "TMC合成") {
         selectgroupPPES.style.display = "none";
+        selectgroupPPES.selectedIndex = 0;
         selectgroupTMC.style.display = "block";
         selectgroupTMC.style.opacity = 1;
     } else {
@@ -42,17 +51,6 @@ selectCompany.addEventListener("change", function () {
         selectgroupTMC.style.opacity = 0.25;
     }
 });
-
-
-//希望納期
-function showSelectedDate() {
-    let datePicker = document.getElementById("datePicker");
-    let selectedDate = new Date(datePicker.value);
-    let options = {
-        year: "numeric", month: "long", day: "numeric"
-    };
-    let formattedDate = selectedDate.toLocaleDateString("ja-JP", options);
-}
 
 
 //工数計算
@@ -112,17 +110,17 @@ function changePage(){
         }
     });
 
-
+    //textareaにチェックボックスにチェックした項目を追加
     document.getElementById("emailContent").value =
-    "依頼元会社と種類は: " + inputCompany + '\n' +
-    "依頼元グループは: " + inputGroupPPES + inputGroupTMC + '\n' +
-    "依頼者は: " + inputRequester + '\n' +
-    "依頼先は: " + inputReceiveGroup + '\n' +
-    "依頼月は: " + inputReceiveMonth + '\n' +
-    "希望納期は: " + inputDate + '\n' +
-    "想定工数は: " + inputTotalArtificial +
-    "(" + inputHuman + "人" + inputTime + "Hr/日" + inputLongdays + "日" + ")" + '\n' +
-    "選択項目は: " + checkedItems.join(', ');
+    " 依頼元会社&種類 : " + inputCompany + '\n' +
+    "  依頼元グループ : " + inputGroupPPES + inputGroupTMC + '\n' +
+    "     依頼者     : " + inputRequester + '\n' +
+    "     依頼先     : " + inputReceiveGroup + '\n' +
+    "     依頼月     : " + inputReceiveMonth + '\n' +
+    "    希望納期    : " + inputDate + '\n' +
+    "    想定工数    : " + inputTotalArtificial +
+    "Hr ( " + inputHuman + " 人 ✖ " + inputTime + " Hr/日 ✖ " + inputLongdays + " 日 " + ")" + '\n' +
+    "    選択項目    : " + checkedItems.join(', ');
 
 }
 
@@ -131,7 +129,6 @@ function changePage(){
 function changeBack(){
     document.getElementById("box00").style.display = "block"
     document.getElementById("box01").style.display = "none"
-
 }
 
 
